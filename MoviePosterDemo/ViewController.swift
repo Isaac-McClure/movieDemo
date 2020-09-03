56db0737a5dd9b1727ae63a996fdf6140b13da6d
Isaac McClure <s3743613@student.rmit.edu.au>
2020-09-03 18:45:18 +1000

    Initial Commit

---
+++ MoviePosterDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviePosterDemo
+//
+//  Created by 1234 on 3/9/20.
+//  Copyright © 2020 NotApplicable. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
